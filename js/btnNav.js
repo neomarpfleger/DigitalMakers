10070dec43a708e4e9ba83aaf37458794f5946d8
neomarpfleger <114037813+neomarpfleger@users.noreply.github.com>
2023-10-07 07:18:13 -0300

    07102023

--- js/btnNav.js
+++ js/btnNav.js
@@ -6,16 +6,19 @@ Remova caso tenha.*/
 const btnMobile =document.getElementById("btnMobile");
 
 function toggleMenu(){
+
+    
+    const icone =document.getElementById("icone")
     const linksNav = document.getElementById("linksNav");
     linksNav.classList.toggle("ative");
 
+    icone.classList.toggle("icone fa-regular fa-sun")
+    
     const imgMobile = document.getElementById("imgMobile");
     imgMobile.classList.toggle("fa-sharp fa-solid fa-ranking-star");
 
-    if(linksNav.classList.contains('ative')){
-        icone.innerHTML = '<i class="icone fa-regular fa-sun"></i> <P>Day</p>'
-    }else{
-        icone.innerHTML = '<i class="icone fa-solid fa-moon"></i> <P>Nigth</p>'
+    if(linksNav.classList.toggle('ative')){
+        icone.innerHTML = '<i class="icone fa-regular fa-sun"></i>'
     }
 }
 
