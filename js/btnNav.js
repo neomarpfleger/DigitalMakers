c87fdf19ebfcf32e7273836097db2b151e9a59da
neomarpfleger <114037813+neomarpfleger@users.noreply.github.com>
2023-10-07 16:25:05 -0300

    07102023
    Ajustes no cabeçario e no rodapé finalizados primeiros testes na versão online.

--- js/btnNav.js
+++ js/btnNav.js
@@ -3,22 +3,18 @@
 Adicione caso não tenha.
 Remova caso tenha.*/
 
-const btnMobile =document.getElementById("btnMobile");
+const btnMobile = document.getElementById("btnMobile");
 
 function toggleMenu(){
 
-    
-    const icone =document.getElementById("icone")
     const linksNav = document.getElementById("linksNav");
     linksNav.classList.toggle("ative");
 
-    icone.classList.toggle("icone fa-regular fa-sun")
-    
-    const imgMobile = document.getElementById("imgMobile");
-    imgMobile.classList.toggle("fa-sharp fa-solid fa-ranking-star");
-
-    if(linksNav.classList.toggle('ative')){
-        icone.innerHTML = '<i class="icone fa-regular fa-sun"></i>'
+    const icone = document.getElementById("icone");
+    if(linksNav.classList == 'ative'){
+        icone.innerHTML = '<i class="fa-solid fa-x"></i>';
+    }else{
+        icone.innerHTML = '<i class="fa-solid fa-bars"></i>';
     }
 }
 
