a6fc9c94aa86789519d24f69ccea18fea2871d73
neomarpfleger <114037813+neomarpfleger@users.noreply.github.com>
2023-10-23 23:53:55 -0300

    22102023
    Adicionado o link correto para pagina de agradecimento.

---
+++ js/verificaCampo.js
@@ -0,0 +1,24 @@
+
+function verificarFormularioPreenchido() {
+    const formulario = document.getElementById("meuFormulario");
+    const camposObrigatorios = formulario.querySelectorAll("[required]");
+
+    for (const campo of camposObrigatorios) {
+        if (!campo.value) {
+            alert("Preencha todos os campos obrigatórios antes de continuar.");
+            return false;
+        }
+    }
+
+    return true;
+}
+
+
+function verificaCampo(){
+
+    var campo = document.getElementById("nome");
+
+    if(campo.value===""){
+        alert("favor preencher este campo");
+    }
+}
